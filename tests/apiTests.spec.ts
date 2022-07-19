1b592d7ada032baeecb867174096f5f7a8898d25
Oleksiy Illiashenko <oleksiy.Illiashenko@titanium-tech.net>
2022-07-19 13:41:58 +0300

    19.07

--- tests/apiTests.spec.ts
+++ tests/apiTests.spec.ts
@@ -3,9 +3,10 @@ import { deleteUser, tokenResponse } from  '../helpers/apiHelpers';
 import { credentialsForAPI } from '../testData/dataForTests';
 import { test } from '@playwright/test';
 
-test('API Login Test', async () => {
-    await tokenResponse (credentialsForAPI.email, credentialsForAPI.password);
+test('API Login Test', async() => {
+await tokenResponse(credentialsForAPI.email, credentialsForAPI.password);
 });
-test('API Delete User', async () => {
-    await deleteUser ();
+
+test('API Delete User', async() => {
+await deleteUser();
 })
--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,29 +1,32 @@
 import { expect, test } from '@playwright/test';
 
 import { Saucedemo } from '../pageObjects/saucedemo';
+import { credentials } from '../testData/dataForTests';
 
-test.describe('3 tests: login, logout, add to card', async () => {
+test.describe('3 tests: login, logout, add to card', async() => {
   let saucedemo: Saucedemo;
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async({ page, baseURL }) => {
     saucedemo = new Saucedemo(page);
-    await page.goto ('https://www.saucedemo.com/')
+    await page.goto('/')
   });
-  test.describe('Saucedemo', async () => {
-    test('Login With Valid Data', async () => {
-await saucedemo.loginWithValidData ();
-await expect(saucedemo.cartBtn).toBeVisible ();
+
+    test('Login With Valid Data', async() => {
+await saucedemo.loginWithValidData(credentials.userName, credentials.password);
+await expect(saucedemo.cartBtn).toBeVisible();
     });
-    test('Logout', async () => {
-    await saucedemo.loginWithValidData ();
-    await saucedemo.sideMenu.click ();
-    await saucedemo.logoutBtn.click ();
-    await expect(saucedemo.loginBtn).toBeVisible ();
+
+    test('Logout', async() => {
+    await saucedemo.loginWithValidData(credentials.userName, credentials.password);
+    await saucedemo.sideMenuBtn.click();
+    await saucedemo.logoutBtn.click();
+    await expect(saucedemo.loginBtn).toBeVisible();
     });
-    test('Adding Product In Cart', async () => {
-    await saucedemo.loginWithValidData ();
-    await saucedemo.addToCartProduct.click ();
-    await saucedemo.cartBtn.click ();
-    await expect(saucedemo.checkAddedProductInCart).toBeVisible ();
+
+    test('Adding Product In Cart', async() => {
+    await saucedemo.loginWithValidData(credentials.userName, credentials.password);
+    await saucedemo.addSauceLabsOnesieToCart.click();
+    await saucedemo.cartBtn.click();
+    await expect(saucedemo.checkAddedProductInCart).toBeVisible();
     });
 })
-});
+
